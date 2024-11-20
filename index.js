14d82329264a00f01be3769f086f4a763a865290
Richard Abishai <rickoshade1891@gmail.com>
2024-11-20 22:59:11 +0530

    projectiles done

--- index.js
+++ index.js
@@ -40,7 +40,30 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+        this.radius = 3
+    }
+
+    draw(){
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = 'white'
+        c.fill()
+        c.closePath()
+    }
+
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
 const player = new Player()
+const projectiles = []
 player.draw()
 
 function animate() {
